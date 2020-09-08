b970f7163a16308c58beb99916ba9e866cccad10
ariwilyan <ariwilyan@gmail.com>
2020-09-08 23:11:02 +0700

    tugas 12

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import Tugas9 from './Tugas-9/Tugas9.js';
-import Tugas10 from './Tugas-10/Tugas10.js';
-import Tugas11 from './Tugas-11/Tugas11.js';
+// import Tugas9 from './Tugas-9/Tugas9.js';
+// import Tugas10 from './Tugas-10/Tugas10.js';
+// import Tugas11 from './Tugas-11/Tugas11.js';
+import Tugas12 from './Tugas-12/Tugas12';
 
 function App() {
   return (
     <>
-      <Tugas9 />
+      {/* <Tugas9 />
       <br/>
       <br/>
       <Tugas10 />
       <br/>
       <br/>
       <Tugas11 />
+      <br/>
+      <br/> */}
+      <Tugas12 />
     </>
   );
 }
